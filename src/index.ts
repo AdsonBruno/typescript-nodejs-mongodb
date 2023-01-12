3f69bb5e84635007bb25d564cf70534cbf7b57d4
AdsonBruno <adsonbruno2@gmail.com>
2023-01-12 14:19:36 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello Word');
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(port, () => console.log(`servidor rodando na porta ${port}!`));
