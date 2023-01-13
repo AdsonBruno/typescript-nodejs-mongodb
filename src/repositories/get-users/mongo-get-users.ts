33266ef7b3ee9988067a2a1d252b3fc761518952
AdsonBruno <adsonbruno2@gmail.com>
2023-01-13 15:55:41 -0300

    feat: ensure GetUsersMongoRepository gets users from mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,17 @@
 import { IGetUsersRepository } from '../../controllers/get-users/protocols';
+import { MongoClient } from '../../database/mongo';
 import { User } from '../../models/user';
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: 'Adson',
-        lastName: 'Bruno',
-        email: 'adsonbruno2@gmail.com',
-        password: '123',
-      },
-    ];
+    const users = await MongoClient.db
+      .collection<Omit<User, 'id'>>('users')
+      .find({})
+      .toArray();
+
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
