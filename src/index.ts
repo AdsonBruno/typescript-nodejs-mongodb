5e99581bab08daf6bfde4f19348802c2347d289a
AdsonBruno <adsonbruno2@gmail.com>
2023-01-14 16:49:38 -0300

    feat: add UpdateUserController with validations

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { MongoGetUsersRepository } from './repositories/get-users/mongo-get-user
 import { MongoClient } from './database/mongo';
 import { MongoCreateUserRepository } from './repositories/create-user/mongo-create-user';
 import { CreateUserController } from './controllers/create-user/create-user';
+import { MongoUpdateUserRepository } from './repositories/update-user/mongo-update-users';
+import { UpdateUserController } from './controllers/update-user/update-user';
 
 const main = async () => {
   config();
@@ -34,6 +36,21 @@ const main = async () => {
 
     const { body, statusCode } = await createUserController.handle({
       body: req.body,
+    });
+
+    res.status(statusCode).send(body);
+  });
+
+  app.patch('/users/:id', async (req, res) => {
+    const mongoUpdateUsersRepository = new MongoUpdateUserRepository();
+
+    const updateUserController = new UpdateUserController(
+      mongoUpdateUsersRepository
+    );
+
+    const { body, statusCode } = await updateUserController.handle({
+      body: req.body,
+      params: req.params,
     });
 
     res.status(statusCode).send(body);
--- src/repositories/update-user/mongo-update-users.ts
+++ src/repositories/update-user/mongo-update-users.ts
@@ -7,7 +7,7 @@ import {
 import { MongoClient } from '../../database/mongo';
 import { User } from '../../models/user';
 
-export class MongoUpdateRepository implements IUpdateUserRepository {
+export class MongoUpdateUserRepository implements IUpdateUserRepository {
   async updateUser(id: string, params: UpdateUserParams): Promise<User> {
     await MongoClient.db.collection('users').updateOne(
       { _id: new ObjectId(id) },
@@ -20,7 +20,7 @@ export class MongoUpdateRepository implements IUpdateUserRepository {
 
     const user = await MongoClient.db
       .collection<Omit<User, 'id'>>('users')
-      .findOne({ _id: new ObjectId() });
+      .findOne({ _id: new ObjectId(id) });
 
     if (!user) {
       throw new Error('User not update');
